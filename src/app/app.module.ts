7d1f9f65ff868ea35b807b1ce901cfc730ee96c0
PoojaGholap94 <72883262+PoojaGholap94@users.noreply.github.com>
2020-10-16 00:13:48 +0530

    Done with designing of Registration Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SignUpModuleComponent } from './sign-up-module/sign-up-module.component';
 import { ThankYouComponent } from './thank-you/thank-you.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,8 @@ import { RegistrationFormComponent } from './registration-form/registration-form
     RegistrationFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
